ad102634de59340693ec1811fa11addffb096c7b
vadim1011 <vadim.nik999@gmail.com>
2025-10-14 11:50:34 +0200

    9

--- proj/src/components/Partners/Partners.jsx
+++ proj/src/components/Partners/Partners.jsx
@@ -9,12 +9,25 @@ const Partners = () => {
   return (
     <section className="partners_sec">
       <div className="partners_title">
-        <h2>Backed by the best</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur dolore adipiscing elit, sed do eiusmod tempor incididunt.</p>
-        <ul>
+        <div>
+           <h2>Backed by the best</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur dolore adipiscing elit, sed do eiusmod tempor incididunt.</p>
+        </div>
+        <ul className='partners_ul'>
           {cards}
         </ul>
+        
       </div>
+      <div>
+          <h2>Never miss a drop</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur dolore adipiscing elit, sed do eiusmod tempor incididunt.
+          </p>
+          <form>
+            <input type="text" />
+            <button type='submit'></button>
+          </form>
+        </div>
     </section>
   )
 }
